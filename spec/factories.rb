92d4cd2d21256753eceabe8e0afa4583ccf61e70
Rod <rodpat@gmail.com>
2017-10-06 17:43:03 +0000

    Adding a detail page for grams

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,8 +3,13 @@ FactoryGirl.define do
   factory :user do
     sequence :email do |n|
       "dummyEmail#{n}@gmail.com"
+    end
+    password "secretPassword"
+    password_confirmation "secretPassword"
   end
-  password "secretPassword"
-  password_confirmation "secretPassword"
+
+  factory :gram do
+    message "hello"
+    association :user
   end
 end
